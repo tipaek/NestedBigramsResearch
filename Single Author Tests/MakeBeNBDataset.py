0cf20c0c841e8aaeab016fe17e8370a184ff40d1
tipaek <tax2farm@gmail.com>
2024-01-11 12:35:55 -0600

    update

--- Single Author Tests/MakeBeNBDataset.py
+++ Single Author Tests/MakeBeNBDataset.py
@@ -4,22 +4,20 @@ sys.path.insert(0, 'C:\\Coding\\Research\\Summer 2023\\Stylometry Neural Network
 from pyod.models.knn import KNN
 from pyod.utils import evaluate_print
 
-from Code.FeatureExtractionCleaned import readFilesWithBERT, readFilesWithEntropy, readFilesWithBERT2
+from Code.FeatureExtractionCleaned import *
 from Code.Helpers import normalize
 
 #file directory containing both regular and anomalous java code
-files = r'C:\Coding\Research\Summer 2023\Stylometry Neural Networks\Data\one'
+files = r'C:\Coding\Research\Summer 2023\Stylometry Neural Networks\Data\15authors'
 
 temp = [10, 20, 30, 40, 50, 60, 70]
 
 #getting features and giving 0/1 classes where 1 is anomalous
 for val in temp:
     print(f'\n\n\n\nVAL: {val}')
-    data = readFilesWithBERT2(files, val)
+    data = NBKL2(files, val)
 
-    data = normalize(data)
-
-    data.to_csv(f'FlCodeBeN.1.{val}.csv', index=False)
+    data.to_csv(f'NBKL2.1.{val}.csv', index=False)
 
 
 
